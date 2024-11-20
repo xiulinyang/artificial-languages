51f1fec3732484a56c24af73bfd08fff299e108f
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-4.local>
2024-11-19 22:16:47 -0500

    update

--- train_lm_transformer.sh
+++ train_lm_transformer.sh
@@ -4,7 +4,7 @@
 GRAMMAR=$1
 SPLIT=$2
 
-fairseq-preprocess --only-source --trainpref "/data_gen/permuted_splits/${GRAMMAR}/${SPLIT}.trn" --validpref "/data_gen/permuted_splits/${GRAMMAR}/${SPLIT}.dev" --testpref "/data_gen/permuted_splits/${GRAMMAR}/${SPLIT}.tst" --destdir "/data-bin/${GRAMMAR}/${SPLIT}-dataset" --workers 20
+fairseq-preprocess --only-source --trainpref "/data_gen/${GRAMMAR/${GRAMMAR}/${SPLIT}.trn" --validpref "/data_gen/${GRAMMAR/${GRAMMAR}/${SPLIT}.dev" --testpref "/data_gen/${GRAMMAR/${GRAMMAR}/${SPLIT}.tst" --destdir "/data-bin/${GRAMMAR}/${SPLIT}-dataset" --workers 20
 
 fairseq-train --task language_modeling "/data-bin/${GRAMMAR}/${SPLIT}-dataset" --save-dir "/checkpoints/${GRAMMAR}/${SPLIT}-transformer" --arch transformer_lm_small --share-decoder-input-output-embed --dropout 0.3 --optimizer adam --adam-betas '(0.9,0.98)' --weight-decay 0.01 --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --clip-norm 0.0 --warmup-init-lr 1e-07 --tokens-per-sample 512 --sample-break-mode none --max-tokens 2048 --update-freq 16 --patience 5 --max-update 10000 --no-epoch-checkpoints --no-last-checkpoints
 
