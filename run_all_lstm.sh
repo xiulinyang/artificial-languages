58044d59892982009bc7b3b36cf7e6e900e68696
xiulinyang <xiulin.yang.compling@gmail.com>
2024-12-04 16:30:27 +0000

    update

--- run_all_lstm.sh
+++ run_all_lstm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for num in {0,9}; do
+for num in {0..9}; do
+    echo "We are running on split ${num}"
     bash train_lm_lstm.sh grammar42 $num
     bash train_lm_lstm.sh grammar42_permutation $num
 done
