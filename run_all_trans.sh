028a4961060fae749efbfe5e7b5d2f05386f25db
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-4.local>
2024-12-04 11:36:52 -0500

    update code

--- run_all_trans.sh
+++ run_all_trans.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for num in {0,9}; do
+mkdir -p trans-results
+
+for num in {0..9}; do
   bash train_lm_transformer.sh grammar42 $num
   bash train_lm_transformer.sh grammar42_permutation $num
 done
